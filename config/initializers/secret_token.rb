5f02fd023f0c627825a3acf4ebcfeb65e794e347
David Mills <dave@1morepx.com>
2013-04-03 14:58:08 -0400

    inital commit
    Basic rails app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spies::Application.config.secret_token = '48f2b4af92b8abab3aace2f91a0cbb9405e5d6f0db07f1ee5fe251add496ff6ddcb0bf7901afce620afeb0ca9244e9b7f6306ee0899b1da86baac1940486a112'
